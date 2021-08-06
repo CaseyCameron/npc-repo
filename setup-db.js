75f12c59ae623f8e8723c36ccba93286ab5c348f
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-05 17:14:22 -0700

    Fixed table creation bug

--- setup-db.js
+++ setup-db.js
@@ -1,3 +1,9 @@
+import app from './lib/app.js';
 import db from './lib/utils/db.js';
 
-db.sync();
+await db.sync({ force: true }).then(result => {
+  console.log(result);
+  app.listen(3000);
+}).catch(err => {
+  console.log(err);
+});;
